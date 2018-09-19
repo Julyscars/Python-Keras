f5d25abe0b903b9bdec06c0eb14670598fe677eb
Julyscars <dolpinsy@gmail.com>
2018-09-19 16:59:32 +0800

    Update 文件列表更新.py

--- 文件列表更新.py
+++ 文件列表更新.py
@@ -1,6 +1,8 @@
 import os
 import pickle
+From Logger import logger 
 file_path = 'd:/new/'
+
 his_ls =[]
 try:
     with open('his.pkl','rb') as f:
@@ -15,9 +17,22 @@ new_ls = [file_path+ i for i in new_ls if str.endswith(i, '.txt') or str.endswit
 news_ls = set(new_ls).difference(set(old_ls))
 news_ls = list(news_ls)
 print(news_ls)
-for i in news_ls:
+for x,i in enumerate(news_ls):
+    #生成从0开始的序列和列表：(0,'string')
     his_ls.append(i)
-if len(news_ls)!=0:
+    logger.info('reading file: {0}/{1}, {2}...'.format(x,len(new_ls),i))
+    try:
+        ############################
+        
+        
+        ############################
+    except Exception as err:
+        ......
+        ......
+        continue
+if len(new_ls) <=0:
+    logger.infor('not new file end...')
+eles:
     with open('his.pkl','wb') as d :
         a = pickle.dump(new_ls,d)
 #print(i)
